b92c0faff35d4471c5443f402c4c7e1ca66eac38
nks102000 <nitishks1110@gmail.com>
2021-10-21 20:45:37 +0530

    Post Request

--- app/src/main/java/nks/api/retrofit/api/SimpleApi.kt
+++ app/src/main/java/nks/api/retrofit/api/SimpleApi.kt
@@ -23,7 +23,7 @@ interface SimpleApi {
 
     @GET("posts")
     suspend fun getCustomPost2(
-        @Query("userd") userId:Int,
+        @Query("userId") userId:Int,
         @QueryMap options:Map<String,String>
     ):Response<List<Post>>
 
